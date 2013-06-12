34cb2a9178cc7e5dc4ba95e8a87bdc9d09fc7b90
Carl-Gustaf Harroch <carl@novoda.com>
2013-06-12 20:06:46 +0100

    getting somewhere very slowly

--- src/main/java/com/novoda/gradle/robolectric/RobolectricPlugin.java
+++ src/main/java/com/novoda/gradle/robolectric/RobolectricPlugin.java
@@ -1,16 +1,10 @@
 package com.novoda.gradle.robolectric;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.concurrent.Callable;
-
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
-import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
@@ -22,23 +16,24 @@ public class RobolectricPlugin implements Plugin<Project> {
     public static final String ANDROID_LIBRARY_PLUGIN_NAME = "android-library";
 
     public static final String COMPILE_CONFIGURATION_NAME = "compile";
-    public static final String ROBOLECTRIC_SOURCE_SET_NAME = "test";
-    public static final String TEST_COMPILE_CONFIGURATION_NAME = "testCompile";
+    public static final String ROBOLECTRIC_SOURCE_SET_NAME = "robolectricTest";
+    public static final String TEST_COMPILE_CONFIGURATION_NAME = "robolectricTestCompile";
     public static final String RUNTIME_CONFIGURATION_NAME = "runtime";
     public static final String TEST_RUNTIME_CONFIGURATION_NAME = "testRuntime";
 
     @Override
     public void apply(Project project) {
-        project.getPlugins().apply(JavaBasePlugin.class);
 
+        project.getPlugins().apply(JavaBasePlugin.class);
         ensureValidProject(project);
 
         JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
-        configureConfigurations(project);
+
         configureSourceSets(javaConvention);
+        configureConfigurations(project);
         configureTest(project, javaConvention);
 
-        //configureBuild(project);
+//        configureBuild(project);
     }
 
     private void ensureValidProject(Project project) {
@@ -52,11 +47,12 @@ public class RobolectricPlugin implements Plugin<Project> {
     void configureConfigurations(Project project) {
         ConfigurationContainer configurations = project.getConfigurations();
         Configuration compileConfiguration = configurations.getByName(COMPILE_CONFIGURATION_NAME);
+
         Configuration runtimeConfiguration = configurations.getByName(RUNTIME_CONFIGURATION_NAME);
         Configuration compileTestsConfiguration = configurations.getByName(TEST_COMPILE_CONFIGURATION_NAME);
         compileTestsConfiguration.extendsFrom(compileConfiguration);
-        configurations.getByName(TEST_RUNTIME_CONFIGURATION_NAME).extendsFrom(runtimeConfiguration, compileTestsConfiguration);
-        configurations.getByName(Dependency.DEFAULT_CONFIGURATION).extendsFrom(runtimeConfiguration);
+//        configurations.getByName(TEST_RUNTIME_CONFIGURATION_NAME).extendsFrom(runtimeConfiguration, compileTestsConfiguration);
+//        configurations.getByName(Dependency.DEFAULT_CONFIGURATION).extendsFrom(runtimeConfiguration);
     }
 
     private void configureSourceSets(final JavaPluginConvention pluginConvention) {
@@ -70,29 +66,26 @@ public class RobolectricPlugin implements Plugin<Project> {
     }
 
     private void configureTest(final Project project, final JavaPluginConvention pluginConvention) {
-
-        project.getTasks().withType(Test.class, new Action<Test>() {
-
-            public void execute(final Test test) {
-                test.getConventionMapping().map("testClassesDir", new Callable<Object>() {
-                    public Object call() throws Exception {
-                        return pluginConvention.getSourceSets().getByName(SourceSet.TEST_SOURCE_SET_NAME).getOutput().getClassesDir();
-                    }
-                });
-                test.getConventionMapping().map("classpath", new Callable<Object>() {
-                    public Object call() throws Exception {
-                        return pluginConvention.getSourceSets().getByName(SourceSet.TEST_SOURCE_SET_NAME).getRuntimeClasspath();
-                    }
-                });
-                test.getConventionMapping().map("testSrcDirs", new Callable<Object>() {
-                    public Object call() throws Exception {
-                        return new ArrayList<File>(pluginConvention.getSourceSets().getByName(SourceSet.TEST_SOURCE_SET_NAME).getJava().getSrcDirs());
-                    }
-                });
-            }
-        });
-
-        Test test = project.getTasks().create("test", Test.class);
+//        project.getTasks().withType(Test.class, new Action<Test>() {
+//            public void execute(final Test test) {
+//                test.getConventionMapping().map("testClassesDir", new Callable<Object>() {
+//                    public Object call() throws Exception {
+//                        return pluginConvention.getSourceSets().getByName(SourceSet.TEST_SOURCE_SET_NAME).getOutput().getClassesDir();
+//                    }
+//                });
+//                test.getConventionMapping().map("classpath", new Callable<Object>() {
+//                    public Object call() throws Exception {
+//                        return pluginConvention.getSourceSets().getByName(SourceSet.TEST_SOURCE_SET_NAME).getRuntimeClasspath();
+//                    }
+//                });
+//                test.getConventionMapping().map("testSrcDirs", new Callable<Object>() {
+//                    public Object call() throws Exception {
+//                        return new ArrayList<File>(pluginConvention.getSourceSets().getByName(SourceSet.TEST_SOURCE_SET_NAME).getJava().getSrcDirs());
+//                    }
+//                });
+//            }
+//        });
+        Test test = project.getTasks().create("robolectricTest", Test.class);
         project.getTasks().getByName(JavaBasePlugin.CHECK_TASK_NAME).dependsOn(test);
         test.setDescription("Runs the unit tests using robolectric.");
         test.setGroup(JavaBasePlugin.VERIFICATION_GROUP);
