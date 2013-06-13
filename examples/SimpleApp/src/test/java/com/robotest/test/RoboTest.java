ec4a11243b4efaa42d8d91ae13b3e0501afeae8d
Carl-Gustaf Harroch <carl@novoda.com>
2013-06-13 20:02:41 +0100

    getting it to run nearly

--- examples/SimpleApp/src/test/java/com/robotest/test/RoboTest.java
+++ examples/SimpleApp/src/test/java/com/robotest/test/RoboTest.java
@@ -1,4 +1,35 @@
 package com.robotest.test;
 
+import android.R;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.robotest.simpleapp.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(org.robolectric.RobolectricTestRunner.class)
 public class RoboTest {
+    private Activity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = new MainActivity();
+        activity.onCreate(null);
+    }
+
+    @Test
+    public void shouldUpdateResultsWhenButtonIsClicked() throws Exception {
+        TextView view = (TextView) activity.findViewById(R.id.hello_world);
+        String resultsText = view.getText().toString();
+        assertThat(resultsText, equalTo("Hello World!"));
+    }
+
+
 }
---
+++ src/main/groovy/com/novoda/gradle/robolectric/RobolectricPlugin.groovy
@@ -0,0 +1,147 @@
+package com.novoda.gradle.robolectric
+
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BasePlugin
+import org.gradle.api.Action
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ConfigurationContainer
+import org.gradle.api.plugins.JavaBasePlugin
+import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.testing.Test
+
+import java.util.concurrent.Callable
+
+class RobolectricPlugin implements Plugin<Project> {
+
+    public static final String ANDROID_PLUGIN_NAME = "android";
+    public static final String ANDROID_LIBRARY_PLUGIN_NAME = "android-library";
+
+    public static final String COMPILE_CONFIGURATION_NAME = "compile";
+    public static final String TEST_COMPILE_CONFIGURATION_NAME = "robolectricTestCompile";
+    public static final String RUNTIME_CONFIGURATION_NAME = "runtime";
+    public static final String TEST_RUNTIME_CONFIGURATION_NAME = "robolectricTestRuntime";
+
+    public static final String ROBOLECTRIC_SOURCE_SET_NAME = "robolectric";
+    public static final String ROBOLECTRIC_CONFIGURATION_NAME = "robolectric";
+    public static final String ROBOLECTRIC_TASK_NAME = "robolectric";
+
+    void apply(Project project) {
+        project.getPlugins().apply(JavaBasePlugin.class);
+        ensureValidProject(project);
+
+        JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
+        configureConfigurations(project);
+        configureSourceSets(javaConvention);
+
+        configureTest(project, javaConvention);
+
+        project.afterEvaluate {
+            configureAndroidDependency(project, javaConvention)
+        }
+    }
+
+    def configureAndroidDependency(Project project, JavaPluginConvention pluginConvention) {
+        SourceSet robolectric = pluginConvention.getSourceSets().findByName(ROBOLECTRIC_SOURCE_SET_NAME);
+
+        ((BasePlugin)getAndroidPlugin(project)).mainSourceSet.getCompileConfigurationName()
+
+        getAndroidPlugin(project).buildTypes.each {
+            it.value.getLocalDependencies().each {
+                robolectric.compileClasspath += files(it.jarFile)
+                robolectric.runtimeClasspath += files(it.jarFile)
+            }
+        }
+
+        // AAR files
+        getAndroidPlugin(project).prepareTaskMap.each {
+            robolectric.compileClasspath += fileTree(dir: it.value.explodedDir, include: '*.jar')
+            robolectric.runtimeClasspath += fileTree(dir: it.value.explodedDir, include: '*.jar')
+        }
+
+        // Default Android jar
+        getAndroidPlugin(project).getRuntimeJarList().each {
+            robolectric.compileClasspath += project.files(it)
+            robolectric.runtimeClasspath += project.files(it)
+        }
+    }
+
+    private void ensureValidProject(Project project) {
+        boolean isAndroidProject = project.getPlugins().hasPlugin(ANDROID_PLUGIN_NAME);
+        boolean isAndroidLibProject = project.getPlugins().hasPlugin(ANDROID_LIBRARY_PLUGIN_NAME);
+        if (!(isAndroidLibProject | isAndroidProject)) {
+            throw new NotAnAndroidProject();
+        }
+    }
+
+    void configureConfigurations(Project project) {
+        ConfigurationContainer configurations = project.getConfigurations();
+        Configuration compileConfiguration = configurations.getByName(COMPILE_CONFIGURATION_NAME);
+        Configuration robolectric = configurations.create(ROBOLECTRIC_CONFIGURATION_NAME);
+        robolectric.extendsFrom(compileConfiguration);
+    }
+
+    private void configureSourceSets(final JavaPluginConvention pluginConvention) {
+        final Project project = pluginConvention.getProject();
+
+        SourceSet robolectric = pluginConvention.getSourceSets().create(ROBOLECTRIC_SOURCE_SET_NAME);
+
+        robolectric.java.srcDir project.file('src/test/java')
+        robolectric.compileClasspath += project.configurations.robolectric
+        robolectric.runtimeClasspath += robolectric.compileClasspath
+    }
+
+    private void configureTest(final Project project, final JavaPluginConvention pluginConvention) {
+        project.getTasks().withType(Test.class, new Action<Test>() {
+            public void execute(final Test test) {
+                configureAndroid();
+                test.getConventionMapping().map("testClassesDir", new Callable<Object>() {
+                    public Object call() throws Exception {
+                        return pluginConvention.getSourceSets().getByName(ROBOLECTRIC_SOURCE_SET_NAME).getOutput().getClassesDir();
+                    }
+                });
+
+                test.getConventionMapping().map("classpath", new Callable<Object>() {
+                    public Object call() throws Exception {
+                        return pluginConvention.getSourceSets().getByName(ROBOLECTRIC_SOURCE_SET_NAME).getRuntimeClasspath();
+                    }
+                });
+
+                test.getConventionMapping().map("testSrcDirs", new Callable<Object>() {
+                    public Object call() throws Exception {
+                        return new ArrayList<File>(pluginConvention.getSourceSets().getByName(ROBOLECTRIC_SOURCE_SET_NAME).getJava().getSrcDirs());
+                    }
+                });
+            }
+
+            def configureAndroid() {
+                project.android.sourceSets.main.java.srcDirs.each { dir ->
+                    def buildDir = dir.getAbsolutePath().split('/')
+                    buildDir = (buildDir[0..(buildDir.length - 4)] + ['build', 'classes', 'debug']).join('/')
+
+                    project.getPlugins().getPlugin('android').prepareTaskMap.each {
+                        project.sourceSets.robolectric.compileClasspath +=
+                                project.files(it.value.explodedDir.getAbsolutePath() + '/classes.jar')
+                        project.sourceSets.robolectric.runtimeClasspath +=
+                                project.files(it.value.explodedDir.getAbsolutePath() + '/classes.jar')
+                    }
+
+                    project.sourceSets.robolectric.compileClasspath += project.files(buildDir)
+                    project.sourceSets.robolectric.runtimeClasspath += project.files(buildDir)
+                }
+            }
+        });
+
+        Test test = project.getTasks().create(ROBOLECTRIC_TASK_NAME, Test.class);
+        project.getTasks().getByName(JavaBasePlugin.CHECK_TASK_NAME).dependsOn(test);
+        test.setDescription("Runs the unit tests using robolectric.");
+        test.setGroup(JavaBasePlugin.VERIFICATION_GROUP);
+    }
+
+    private AppPlugin getAndroidPlugin(Project project) {
+        return (AppPlugin) project.getPlugins().findPlugin(ANDROID_PLUGIN_NAME);
+    }
+
+}
