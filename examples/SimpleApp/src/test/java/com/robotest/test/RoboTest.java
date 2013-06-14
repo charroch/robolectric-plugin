9595bdf362212d3870619fe14001bd3c0f199660
Adam <adam@novoda.com>
2013-06-14 10:21:32 +0100

    upping robolectric version, fixing test producing null pointer
    really needs some ide support

--- examples/SimpleApp/src/test/java/com/robotest/test/RoboTest.java
+++ examples/SimpleApp/src/test/java/com/robotest/test/RoboTest.java
@@ -1,32 +1,32 @@
 package com.robotest.test;
 
 import android.app.Activity;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.robotest.simpleapp.MainActivity;
+import com.robotest.simpleapp.R;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.robolectric.*;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-@RunWith(org.robolectric.RobolectricTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class RoboTest {
-    private Activity activity;
-
-    @Before
-    public void setUp() throws Exception {
-        activity = new MainActivity();
-    }
 
     @Test
     public void shouldUpdateResultsWhenButtonIsClicked() throws Exception {
-        TextView view = (TextView) activity.findViewById(com.robotest.simpleapp.R.id.hello_world);
-        String resultsText = view.getText().toString();
-        assertThat(resultsText, equalTo("Hello World!"));
+        String testText = "Hello World!";
+        TextView view = new TextView(Robolectric.application);
+
+        view.setText(testText);
+	    String viewText = view.getText().toString();
+
+        assertThat(viewText, equalTo(testText));
     }
 
 
